ffa51e7712edb27825e580ad1f3beb13a4b973ec
vihuynhx <eaamvth@students.eaaa.dk>
2023-04-19 11:05:43 +0200

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//SLIDESHOW MED JEANETTE//
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -37,3 +38,41 @@ function showSlides(n) {
     document.querySelector(".frem").style.display = "block";
   }
 }
+
+//GODE RÅD///
+
+const rad1 = document.getElementById("rad1");
+const rad2 = document.getElementById("rad2");
+const rad3 = document.getElementById("rad3");
+const rad4 = document.getElementById("rad4");
+
+const hojrePil = document.getElementById("hojrePil");
+const venstrePil = document.getElementById("venstrePil");
+
+// Arrayet:
+const carousel = [rad1, rad2, rad3, rad4];
+
+carousel[0].style.display = "block";
+carousel[1].style.display = "none";
+carousel[2].style.display = "none";
+carousel[3].style.display = "none";
+
+// Knapper:
+hojrePil.addEventListener("click", naesteBillede);
+venstrePil.addEventListener("click", forrigeBillede);
+
+// Funktioner:
+function naesteBillede(){
+    carousel[0].style.display = "none"; // Skjul den viste
+    carousel.push(carousel[0]); // Put den første ind til sidst
+    carousel.shift(); // Fjern den første og flyt alle en ned
+    carousel[0].style.display = "block"; // Vis den der nu er først
+}
+
+function forrigeBillede(){
+    carousel[0].style.display = "none";
+    carousel.unshift(carousel[3]);
+    carousel.pop();
+    carousel[0].style.display = "block";
+}
+
